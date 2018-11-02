379082387931ab273b604e3d45bce929df4a1c3e
Bjarte Stien Karlsen <bjarte.karlsen@skatteetaten.no>
2018-11-02 15:06:54 +0100

    add headerfilter as property

--- src/main/java/no/skatteetaten/aurora/config/AuroraProperties.java
+++ src/main/java/no/skatteetaten/aurora/config/AuroraProperties.java
@@ -5,6 +5,16 @@ import org.springframework.boot.context.properties.ConfigurationProperties;
 @ConfigurationProperties(prefix = "aurora.starter")
 public class AuroraProperties {
 
+    private Headerfilter headerfilter;
+
+    public Headerfilter getHeaderfilter() {
+        return headerfilter;
+    }
+
+    public void setHeaderfilter(Headerfilter headerfilter) {
+        this.headerfilter = headerfilter;
+    }
+
     public static class Headerfilter {
         /*
          * Set to false to turn of header filter
